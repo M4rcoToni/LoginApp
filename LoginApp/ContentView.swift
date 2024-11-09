229c293278c813880707fb22bb85e9decbbcb8e1
m4rcotoni <marcosanto.sl021@academico.ifsul.edu.br>
2024-11-09 02:43:30 -0300

    new desing

--- LoginApp/ContentView.swift
+++ LoginApp/ContentView.swift
@@ -12,67 +12,96 @@ import FirebaseAuth
 struct ContentView: View {
     @State private var email = ""
     @State private var password = ""
+    @State private var user = ""
+    @FocusState private var emailInput: Bool
     var body: some View {
         ZStack {
             Color.black
                 
             RoundedRectangle(cornerRadius: 20, style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/)
-                .foregroundStyle(.linearGradient(colors: [.white,.gray], startPoint: .topLeading, endPoint: .bottomTrailing))
+                .foregroundStyle(
+                    .linearGradient(
+                        colors: [.white,.gray], 
+                        startPoint: .topLeading,
+                        endPoint: .bottomTrailing
+                    )
+                )
                 .frame(width: 1000, height: 120)
                 .rotationEffect(.degrees(135))
                 .blur(radius: 180)
           
-            VStack(spacing:20) {
-                Text("Welcome")
-                    .foregroundStyle(.white)
-                    .font(.system(size: 40, weight: .bold, design: .serif))
-                
+            VStack() {
+                VStack(alignment: .leading) {
+                    Text("Welcome back")
+                        .foregroundColor(.white)
+                        .font(.system(size: 32, weight: .bold, design: .serif))
+                    
+                    Text("Sign in to your account")
+                        .foregroundColor(.white)
+                        .opacity(0.7)
+                        .font(.system(size: 16, weight: .medium, design: .serif))
+                }
+                .padding(.bottom, 32.0)
             
-                TextField("Email",text: $email)
-                    .foregroundColor(.white)
-                    .placeholder(when: email.isEmpty){
-                        Text("Email")
-                            .foregroundStyle(.white)
-                            .font(.system(size: 16, weight: .medium, design: .serif))
-                    }
                 
+                VStack(alignment: .leading) {
+                    Text("Email")
+                        .foregroundColor(.white)
+                        .font(.system(size: 16, weight: .medium, design: .serif))
+                    
+                    TextField("Email", text: $email)
+                        .focused($emailInput)
+                        .foregroundColor(.white)
+                        .padding(8)
+                        .background(
+                            RoundedRectangle(cornerRadius: 8,style: .continuous)
+                                .foregroundColor(.gray)
+                                .border(Color.white, width: emailInput ? 1 : 0)
+                        )
+                        .font(.system(size: 16, weight: .medium, design: .serif))
+                        
+                }
                 
-                Rectangle()
-                    .frame(width: 350,height: 1)
-                    .foregroundColor(.white)
-                
-                SecureField("Password", text: $password)
-                    .foregroundColor(.white)
-                    .textFieldStyle(.plain)
-                    .placeholder(when: password.isEmpty){
-                        Text("Password")
-                            .foregroundStyle(.white)
-                            .font(.system(size: 16, weight: .medium, design: .serif))
-                    }
-                
-                Rectangle()
-                    .frame(width: 350,height: 1)
-                    .foregroundColor(.white)
-            
+                VStack(alignment: .leading) {
+                    Text("Password")
+                        .foregroundColor(.white)
+                        .font(.system(size: 16, weight: .medium, design: .serif))
+                    
+                    SecureField("Password", text: $password)
+                        .foregroundColor(.white)
+                        .textFieldStyle(.plain)
+                        .padding(8)
+                        .background(
+                            RoundedRectangle(cornerRadius: 8,style: .continuous)
+                                .foregroundColor(.gray)
+                        )
+                        .font(.system(size: 16, weight: .medium, design: .serif))
+                    
+                        
+                }.padding(.top, 4)
                 
                 Button {
-                    register()
+                    login()
                 } label: {
                     Text("SignUp")
                         .bold()
-                        .frame(width: 200, height: 40)
-                        .background(
-                            RoundedRectangle(cornerRadius: 12,style: .continuous)
-                                .fill(.linearGradient(colors: [.gray,.white], startPoint: .topLeading, endPoint: .trailing))
-                        )
                         .foregroundColor(.black)
                         .font(.system(size: 16, weight: .medium, design: .serif))
                     
                 }
+                .frame(width: 200,height: 40)
+                .background(
+                    RoundedRectangle(cornerRadius: 12,style: .continuous)
+                        .fill(.linearGradient(colors: [.gray,.white], startPoint: .topLeading, endPoint: .trailing))
+                )
+                .padding(.top, 32)
+                
+                
 
                 
             }
-            .frame(width: 350)
+            .frame(width: 390)
+    
             
         
         }
@@ -83,6 +112,7 @@ struct ContentView: View {
             if error != nil{
                 print(error!.self)
             }
+            user = result?.user.email ?? ""
         }
     }
     
